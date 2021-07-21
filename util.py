00d1032940756b61e060e2b21b32bfe2de6bfe82
kpstesla <kswhitecross@gmail.com>
2021-07-21 15:59:40 -0400

    look at some common ML baselines

--- util.py
+++ util.py
@@ -23,7 +23,9 @@ class LegoDataset(Dataset):
         self.transform = transform
         self.target_transform = target_transform
         self.test = test
-        self.img_dir = os.path.join(img_dir, "test/" if test else "train/")
+        self.img_dir = img_dir
+        self.specific_dir = os.path.join(img_dir, 'test/') if test else os.path.join(img_dir, 'train/')
+        self.adjusted_dir = img_dir if test else self.specific_dir
         self.full_df = None
 
         # if data not present, then download and format it
@@ -50,12 +52,12 @@ class LegoDataset(Dataset):
                 shutil.move(os.path.join(img_dir, f), os.path.join(img_dir, 'train/', f))
 
         # read path names and class names from csv files
-        meta_df = pd.read_csv(os.path.join(self.img_dir, "metadata.csv"))
+        meta_df = pd.read_csv(os.path.join(self.specific_dir, "metadata.csv"))
         if self.test:
-            test_df = pd.read_csv(os.path.join(self.img_dir, "test.csv"))
+            test_df = pd.read_csv(os.path.join(self.specific_dir, "test.csv"))
             self.full_df = test_df.merge(meta_df, on="class_id")
         else:
-            train_df = pd.read_csv(os.path.join(self.img_dir, "index.csv"))
+            train_df = pd.read_csv(os.path.join(self.specific_dir, "index.csv"))
             self.full_df = train_df.merge(meta_df, on="class_id")
 
     def __len__(self):
@@ -63,7 +65,7 @@ class LegoDataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.full_df.iloc[idx]
-        image = read_image(os.path.join(self.img_dir, row["path"])).float()
+        image = read_image(os.path.join(self.adjusted_dir, row["path"])).float()
         label = row["class_id"] - 1
         if self.transform:
             image = self.transform(image)
